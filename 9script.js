1335856696656e5a27b96b035d294fe95306a1b8
Princep712 <166273615+Princep712@users.noreply.github.com>
2025-07-14 18:52:18 +0530

    Add files via upload

---
+++ 9script.js
@@ -0,0 +1,23 @@
+// 1. Declare variables
+let name = "Prince Patel";
+let age = 21;
+let city = "Ahmedabad";
+let isStudent = true;
+
+// 2. Perform arithmetic
+let num1 = 10;
+let num2 = 5;
+let sum = num1 + num2;
+let difference = num1 - num2;
+
+// 3. Comparison operator
+let isAdult = age > 18;
+
+// 4. Log all values in the console
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("City:", city);
+console.log("Is a Student:", isStudent);
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Is age greater than 18?", isAdult);
